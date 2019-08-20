cd9f416c90f193eeda36550013d1e6e0498677a5
saviour2008 <saviour2008@126.com>
2019-08-20 13:16:30 +0800

    [FEATURE] add customer style

--- src/views/standardVerify/templeteVariableManagement.vue
+++ src/views/standardVerify/templeteVariableManagement.vue
@@ -1,30 +1,44 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+  <div class>
+     <div class="page-title">模板变量列表</div>
+
+     <div class="paddingh20">
+       <el-card>
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="系统信息" name="first">系统信息</el-tab-pane>
+          <el-tab-pane label="输入信息" name="second">输入信息</el-tab-pane>
+          <el-tab-pane label="表格信息" name="third">表格信息</el-tab-pane>
+        </el-tabs>
+      </el-card>
+     </div>
+     
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
-export default {
-  name: 'TempleteVariableManagement',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  export default {
+    name: 'TempleteVariableManagement',
+    data() {
+      return {
+        activeName: 'second'
+      }
+    },
+    created() {
+       
+    },
+    mounted() {
+      
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
   }
-}
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
+<style>
+
 </style>
+
